e983cbcca89deb20dbceb88635c4a17595ab813b
matheusperoba <60474417+matheusperoba@users.noreply.github.com>
2022-02-14 19:49:10 -0300

    JS

---
+++ JS/operadores/operadores.js
@@ -0,0 +1,16 @@
+//precedencia
+
+var num1 = 9
+var num2 = 2
+
+document.write(`A soma entre ${num1} e ${num2} é: `+(num1+num2) + '<br/>')
+document.write(`A subtração entre ${num1} e ${num2} é: `+(num1-num2)+'<br/>')
+document.write(`A multiplicação entre ${num1} e ${num2} é: `+(num1*num2)+'<br/>')
+document.write(`A divisao entre ${num1} e ${num2} é: `+(num1/num2)+'<br/>')
+document.write(`O modulo entre ${num1} e ${num2} é: `+(num1%num2)+'<br/>')
+document.write(`O incremento de ${num1} é :${++num1}`+'<br/>')
+document.write(`O decremento de ${num2} é :${++num2}`+'<br/>')
+
+
+
+
